0fc0b5464447ddfe09a3435d128aaae954e08f33
manux21 <manux21@yandex.ru>
2023-12-10 21:26:55 +0300

    changes

---
+++ include/OtaCicd.h
@@ -0,0 +1,38 @@
+#ifndef OTA_CICD_H
+#define OTA_CICD_H
+
+#include <Arduino.h>
+#include <Preferences.h>
+#include <ArduinoJson.h>
+#include <HttpsOTAUpdate.h>
+#include <esp_https_ota.h>
+#include <esp_http_client.h>
+#include <mqtt_client.h>
+
+struct ReleaseMessage
+{
+    String repository;
+    String version;
+    String url;
+};
+
+class OtaCicd
+{
+public:
+    static bool init(String certPem);
+    static bool init(String certPem, String releaseTopic, esp_mqtt_client_config_t mqttConfig);
+    static void start(String message);
+    static String getVersion();
+    static esp_mqtt_client_handle_t mqttClient;
+    String getCurrentVersion();
+
+private:
+    static String _certPem;
+    static String _releaseTopic;
+    static Preferences _preferences;
+    static bool _setVersion(String version);
+    static ReleaseMessage _parseMessage(String message);
+    static void _onMqttData(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+};
+
+#endif
---
+++ lib/OtaCicd.cpp
@@ -0,0 +1,215 @@
+#include "OtaCicd.h"
+#include <ESP32httpUpdate.h>
+
+
+String OtaCicd::_certPem;
+String OtaCicd::_releaseTopic;
+Preferences OtaCicd::_preferences;
+esp_mqtt_client_handle_t OtaCicd::mqttClient;
+
+bool OtaCicd::init(String certPem)
+{
+    if (_preferences.begin("ota-cicd"))
+    {
+        String currentVersion = getVersion();
+        Serial.printf("[ota-cicd] detected version %s \n", currentVersion);
+
+        _certPem = certPem;
+        return true;
+    }
+
+    return false;
+}
+
+bool OtaCicd::init(String certPem, String releaseTopic, esp_mqtt_client_config_t mqttConfig)
+{
+    mqttClient = esp_mqtt_client_init(&mqttConfig);
+
+    if (mqttClient == NULL)
+    {
+        Serial.printf("[ota-cicd] unable to create MQTT client \n");
+        return false;
+    }
+
+    esp_err_t err = esp_mqtt_client_register_event(mqttClient, MQTT_EVENT_ANY, _onMqttData, mqttClient);
+
+    if (err != ESP_OK)
+    {
+        Serial.printf("[ota-cicd] unable to register event handler \n");
+        return false;
+    }
+
+    err = esp_mqtt_client_start(mqttClient);
+
+    if (err != ESP_OK)
+    {
+        Serial.printf("[ota-cicd] unable to start MQTT client \n");
+        return false;
+    }
+
+    _releaseTopic = releaseTopic;
+
+    return init(certPem);
+}
+
+
+String OtaCicd::getCurrentVersion()
+{
+    return getVersion();
+}
+
+void OtaCicd::start(String message)
+{
+    ReleaseMessage releaseMessage = _parseMessage(message);
+
+    String releaseVersion = releaseMessage.version;
+    String currentVersion = getVersion();
+
+    Serial.printf("release message ", releaseMessage, "\n");
+    Serial.printf("currentVersion ", currentVersion, "\n");
+    Serial.printf("releaseVersion ", releaseVersion, "\n");
+
+    if (releaseVersion == currentVersion)
+    {
+        Serial.printf("[otaCicd] firmware up to date \n");
+        return;
+    }
+
+    
+
+    Serial.printf("[otaCicd] new version found %s \n", releaseVersion);
+
+    // HttpsOTA.onHttpEvent([](HttpEvent_t *event) {});
+    Serial.printf("Downloading firmware from URL: %s\n", releaseMessage.url.c_str());
+    // HttpsOTA.begin(releaseMessage.url.c_str(), _certPem.c_str());
+
+    bool otaRunning = true;
+    bool updateSuccess = false;
+
+    while (otaRunning)
+    {
+        
+        t_httpUpdate_return ret = ESPhttpUpdate.update(releaseMessage.url.c_str());
+
+        switch (ret)
+        {
+            case HTTP_UPDATE_FAILED:
+                Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+                otaRunning = false;
+                break;
+
+            case HTTP_UPDATE_NO_UPDATES:
+                Serial.println("HTTP_UPDATE_NO_UPDATES");
+                otaRunning = false;
+                break;
+
+            case HTTP_UPDATE_OK:
+                Serial.println("HTTP_UPDATE_OK");
+                otaRunning = false;
+                updateSuccess = true;
+                break;
+        }
+
+        delay(1000);
+    }
+
+    // while (otaRunning)
+    // {
+    //     switch (HttpsOTA.status())
+    //     {
+    //     case HTTPS_OTA_IDLE:
+    //         Serial.printf("[otaCicd] update have not started yet \n");
+    //         break;
+
+    //     case HTTPS_OTA_UPDATING:
+    //         Serial.printf("[otaCicd] update in progress \n");
+    //         break;
+
+    //     case HTTPS_OTA_SUCCESS:
+    //         Serial.printf("[otaCicd] update is successful \n");
+    //         otaRunning = false;
+    //         updateSuccess = true;
+    //         break;
+
+    //     case HTTPS_OTA_FAIL:
+    //         Serial.printf("[otaCicd] update failed \n");
+    //         otaRunning = false;
+    //         break;
+
+    //     case HTTPS_OTA_ERR:
+    //         Serial.printf("[otaCicd] error occured while creating xEventGroup() \n");
+    //         otaRunning = false;
+    //         break;
+    //     }
+
+    //     delay(1000);
+    // }
+
+    if (updateSuccess)
+    {
+        _setVersion(releaseVersion);
+        ESP.restart();
+    }
+}
+
+String OtaCicd::getVersion()
+{
+    return _preferences.getString("version", "unknown");
+}
+
+bool OtaCicd::_setVersion(String version)
+{
+    if (_preferences.putString("version", version) > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+ReleaseMessage OtaCicd::_parseMessage(String message)
+{
+    StaticJsonDocument<300> doc;
+    DeserializationError error = deserializeJson(doc, message);
+
+    if (error)
+    {
+        Serial.printf("[otaCicd] failed to parse message \n");
+    }
+
+    ReleaseMessage releaseMessage;
+
+    releaseMessage.repository = doc["repository"].as<String>();
+    releaseMessage.url = doc["url"].as<String>();
+    releaseMessage.version = doc["tag"].as<String>();
+
+    return releaseMessage;
+}
+
+void OtaCicd::_onMqttData(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = *((esp_mqtt_event_handle_t *)(&event_data));
+    esp_mqtt_client_handle_t client = event->client;
+
+    switch (event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        Serial.printf("[ota-cicd] mqtt client connected \n");
+        esp_mqtt_client_subscribe(client, _releaseTopic.c_str(), 2);
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        Serial.printf("[ota-cicd] mqtt client disconnected \n");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        Serial.printf("[ota-cicd] mqtt client subscribed to release topic\n");
+        break;
+
+    case MQTT_EVENT_DATA:
+        if (strcmp(event->topic, _releaseTopic.c_str()) == 0)
+        {
+            start(event->data);
+        }
+        break;
+    }
+}
