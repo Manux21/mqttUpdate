b0d9c9667bc1d8630f484f594a9fc2f0d4ac81db
manux21 <manux21@yandex.ru>
2024-05-29 03:08:23 +0300

    new pipe

---
+++ notify_mqtt.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+import os
+
+broker_address = os.getenv("MQTT_BROKER_ADDRESS")
+port = int(os.getenv("MQTT_PORT", 1883))
+topic = "esp32/update"
+
+client = mqtt.Client("GitHubActionsClient")
+client.username_pw_set(os.getenv("MQTT_USERNAME"), os.getenv("MQTT_PASSWORD"))
+client.connect(broker_address, port=port)
+
+message = {
+    "firmware_url": f"https://{os.getenv('S3_BUCKET')}.s3.amazonaws.com/firmware/firmware.bin",
+    "version": os.getenv("FIRMWARE_VERSION", "1.0.0")
+}
+
+client.publish(topic, str(message))
+client.disconnect()
